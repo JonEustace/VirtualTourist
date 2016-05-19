a45caaf0f262b1066a64747dc7a38f846c87b134
Jonathan Eustace <jeustace@tangledpixel.ca>
2016-05-19 11:23:32 -0700

    Initial Commit

---
+++ VirtualTourist2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist2
+//
+//  Created by iMac on 2016-05-19.
+//  Copyright © 2016 iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
